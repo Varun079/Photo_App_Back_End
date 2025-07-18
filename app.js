e2e35d53f44f1463994aa26fd2f1eabdb0fed2e2
VARUN\varun <varunkrishnadasari@gmail.com>
2025-07-18 09:52:45 +0530

    App

--- app.js
+++ app.js
@@ -12,13 +12,19 @@ const app = express();
 
 app.use(morgan("dev")); // global middleware
 
-app.use(
-    cors({
-        origin: process.env.FRONTEND_URL,
-        credentials: true,
-    })
-);
-
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    "http://localhost:5173",
+    "https://photo-app-front-end-92lk.vercel.app"
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
 
