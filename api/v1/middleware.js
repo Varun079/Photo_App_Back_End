b4535743caea76e3fcd88a9c1b27902db9204bfb
VARUN\varun <varunkrishnadasari@gmail.com>
2025-07-18 10:37:07 +0530

    App

--- api/v1/middleware.js
+++ api/v1/middleware.js
@@ -5,13 +5,12 @@ const userAuthenticationMiddleware = (req, res, next) => {
     console.log("--> inside userAuthenticationMiddleware");
     try {
         const { authorization } = req.cookies;
-        // console.log("--> authorization", authorization);
         if (!authorization) {
-            res.status(401).json({ isSuccess: false, message: "Token not found!" });
+            return res.status(401).json({ isSuccess: false, message: "Token not found!" });
         }
         jwt.verify(authorization, process.env.JWT_SECRET, function (err, decodedData) {
             if (err) {
-                res.status(401).json({
+                return res.status(401).json({
                     isSuccess: false,
                     message: "Invalid token!",
                     data: {},
